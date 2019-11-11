9e2b05eed09255979ba4bc1d89169c770623ab4f
Dženan Zukić <dzenan.zukic@kitware.com>
2019-11-11 11:32:50 -0500

    WIP: segmentation works-ish
    Some over-segmentation
    All the bones are merged

--- segmentBonesInMicroCT.cxx
+++ segmentBonesInMicroCT.cxx
@@ -6,13 +6,15 @@
 #include "itkBinaryThresholdImageFilter.h"
 #include "itkConnectedComponentImageFilter.h"
 #include "itkRelabelComponentImageFilter.h"
+#include "itkSignedMaurerDistanceMapImageFilter.h"
+#include "itkNotImageFilter.h"
 #include "itkMultiScaleHessianEnhancementImageFilter.h"
 #include "itkDescoteauxEigenToScalarImageFilter.h"
 
 
 template <typename TImage>
 void
-Write(const TImage * out, std::string filename, bool compress)
+WriteImage(const TImage * out, std::string filename, bool compress)
 {
   using WriterType = itk::ImageFileWriter<TImage>;
   typename WriterType::Pointer w = WriterType::New();
@@ -31,30 +33,93 @@ Write(const TImage * out, std::string filename, bool compress)
 
 template <typename TImage>
 void
-Write(itk::SmartPointer<TImage> out, std::string filename)
+WriteImage(itk::SmartPointer<TImage> out, std::string filename)
 {
-  Write(out.GetPointer(), filename.c_str());
+  WriteImage(out.GetPointer(), filename.c_str());
 }
 
+// split the binary mask into components and remove the small islands
 template <typename TImage>
 itk::SmartPointer<TImage>
-connectedComponentAnalysis(const TImage * labelImage, std::string outFilename)
+connectedComponentAnalysis(const TImage * labelImage, std::string outFilename, itk::IdentifierType & numLabels)
 {
-  using LabelImageType = itk::Image<itk::SizeValueType, TImage::ImageDimension>;
-  using LabelerType = itk::ConnectedComponentImageFilter<TImage, LabelImageType>;
+  using ManyLabelImageType = itk::Image<itk::SizeValueType, TImage::ImageDimension>;
+  using LabelerType = itk::ConnectedComponentImageFilter<TImage, ManyLabelImageType>;
   LabelerType::Pointer labeler = LabelerType::New();
   labeler->SetInput(labelImage);
   static unsigned invocationCount = 0;
-  Write(labeler->GetOutput(), outFilename + std::to_string(invocationCount) + "-cc-label.nrrd", true);
+  WriteImage(labeler->GetOutput(), outFilename + std::to_string(invocationCount) + "-cc-label.nrrd", true);
 
-  using RelabelType = itk::RelabelComponentImageFilter<LabelImageType, TImage>;
+  using RelabelType = itk::RelabelComponentImageFilter<ManyLabelImageType, TImage>;
   typename RelabelType::Pointer relabeler = RelabelType::New();
   relabeler->SetInput(labeler->GetOutput());
   relabeler->SetMinimumObjectSize(1000);
-  Write(relabeler->GetOutput(), outFilename + std::to_string(invocationCount) + "-ccR-label.nrrd", true);
+  WriteImage(relabeler->GetOutput(), outFilename + std::to_string(invocationCount) + "-ccR-label.nrrd", true);
   ++invocationCount;
 
+  relabeler->Update();
+  numLabels = relabeler->GetNumberOfObjects();
   return relabeler->GetOutput();
+}
+
+
+// morphological dilation by thresholding the distance field
+template <typename TImage>
+itk::SmartPointer<TImage>
+sdfDilate(itk::SmartPointer<TImage> labelImage, double radius, std::string outFilename)
+{
+  using RealPixelType = float;
+  using RealImageType = itk::Image<RealPixelType, TImage::ImageDimension>;
+  using DistanceFieldType = itk::SignedMaurerDistanceMapImageFilter<TImage, RealImageType>;
+
+  typename DistanceFieldType::Pointer distF = DistanceFieldType::New();
+  distF->SetInput(labelImage);
+  distF->SetSquaredDistance(true);
+  static unsigned invocationCount = 0;
+  WriteImage(distF->GetOutput(), outFilename + std::to_string(invocationCount) + "-dist-dilate.nrrd", false);
+
+  using FloatThresholdType = itk::BinaryThresholdImageFilter<RealImageType, TImage>;
+  typename FloatThresholdType::Pointer sdfTh = FloatThresholdType::New();
+  sdfTh->SetInput(distF->GetOutput());
+  sdfTh->SetUpperThreshold(radius * radius);
+  WriteImage(sdfTh->GetOutput(), outFilename + std::to_string(invocationCount) + "-dilate-label.nrrd", true);
+  ++invocationCount;
+
+  sdfTh->Update();
+  return sdfTh->GetOutput();
+}
+
+// morphological erosion by thresholding the distance field
+template <typename TImage>
+itk::SmartPointer<TImage>
+sdfErode(itk::SmartPointer<TImage> labelImage, double radius, std::string outFilename)
+{
+  // we need an inversion filter because Maurer's filter distances are not symmetrical
+  // inside distances start at 0, while outside distances start at single spacing
+  using NotType = itk::NotImageFilter<TImage, TImage>;
+  NotType::Pointer negator = NotType::New();
+  negator->SetInput(labelImage);
+  static unsigned invocationCount = 0;
+  WriteImage(negator->GetOutput(), outFilename + std::to_string(invocationCount) + "-erode-Not-label.nrrd", true);
+
+  using RealPixelType = float;
+  using RealImageType = itk::Image<RealPixelType, TImage::ImageDimension>;
+  using DistanceFieldType = itk::SignedMaurerDistanceMapImageFilter<TImage, RealImageType>;
+
+  typename DistanceFieldType::Pointer distF = DistanceFieldType::New();
+  distF->SetInput(negator->GetOutput());
+  distF->SetSquaredDistance(true);
+  WriteImage(distF->GetOutput(), outFilename + std::to_string(invocationCount) + "-dist-erode.nrrd", false);
+
+  using FloatThresholdType = itk::BinaryThresholdImageFilter<RealImageType, TImage>;
+  typename FloatThresholdType::Pointer sdfTh = FloatThresholdType::New();
+  sdfTh->SetInput(distF->GetOutput());
+  sdfTh->SetLowerThreshold(radius * radius);
+  WriteImage(sdfTh->GetOutput(), outFilename + std::to_string(invocationCount) + "-erode-label.nrrd", true);
+  ++invocationCount;
+
+  sdfTh->Update();
+  return sdfTh->GetOutput();
 }
 
 template <typename ImageType>
@@ -62,19 +127,25 @@ void
 mainProcessing(typename ImageType::ConstPointer inImage, std::string outFilename, const itk::Array<double> & sigmaArray)
 {
   constexpr unsigned ImageDimension = ImageType::ImageDimension;
-  using OutImageType = itk::Image<unsigned char, ImageDimension>;
-  using BinaryThresholdType = itk::BinaryThresholdImageFilter<ImageType, OutImageType>;
+  using LabelImageType = itk::Image<unsigned char, ImageDimension>;
+  using BinaryThresholdType = itk::BinaryThresholdImageFilter<ImageType, LabelImageType>;
 
   typename BinaryThresholdType::Pointer binTh = BinaryThresholdType::New();
   binTh->SetInput(inImage);
   binTh->SetLowerThreshold(1000);
-  Write(binTh->GetOutput(), outFilename + "-bin1-label.nrrd", true);
+  WriteImage(binTh->GetOutput(), outFilename + "-bin1-label.nrrd", true);
 
-  typename OutImageType::Pointer thBone = connectedComponentAnalysis(binTh->GetOutput(), outFilename);
+  itk::IdentifierType numBones = 0;
+
+  typename LabelImageType::Pointer thBone = connectedComponentAnalysis(binTh->GetOutput(), outFilename, numBones);
+  typename LabelImageType::Pointer dilatedBone =
+    sdfDilate(thBone, 3.0 * sigmaArray[sigmaArray.size() - 1], outFilename);
+  typename LabelImageType::Pointer erodedBone =
+    sdfErode(dilatedBone, 3.5 * sigmaArray[sigmaArray.size() - 1], outFilename);
+
 
   using RealPixelType = float;
   using RealImageType = itk::Image<RealPixelType, ImageDimension>;
-  // SDF
 
   using MultiScaleHessianFilterType = itk::MultiScaleHessianEnhancementImageFilter<ImageType, RealImageType>;
   using DescoteauxEigenToScalarImageFilterType =
@@ -88,7 +159,7 @@ mainProcessing(typename ImageType::ConstPointer inImage, std::string outFilename
   multiScaleFilter->SetSigmaArray(sigmaArray);
 
   // multiScaleFilter->Update();
-  // Write(multiScaleFilter->GetOutput(), outFilename + "-desco.nrrd", false);
+  // WriteImage(multiScaleFilter->GetOutput(), outFilename + "-desco.nrrd", false);
 }
 
 int
