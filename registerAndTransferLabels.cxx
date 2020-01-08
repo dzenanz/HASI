8f8a0bbac7b386f35e4457dad939d48a5fc77051
Dženan Zukić <dzenan.zukic@kitware.com>
2020-01-08 13:28:13 -0500

    Add fiducial reading

--- registerAndTransferLabels.cxx
+++ registerAndTransferLabels.cxx
@@ -46,6 +46,55 @@ WriteImage(itk::SmartPointer<TImage> out, std::string filename, bool compress)
   std::cout << diff.count() << " Done!" << std::endl;
 }
 
+std::vector<itk::Point<double, 3>>
+readSlicerFiducials(std::string fileName)
+{
+  using PointType = itk::Point<double, 3>;
+  std::ifstream pointsFile(fileName.c_str());
+  std::string   line;
+  // ignore first 3 lines (comments of fiducials savefile)
+  std::getline(pointsFile, line); //# Markups fiducial file version = 4.10
+  std::getline(pointsFile, line); //# CoordinateSystem = 0
+  bool ras = false;
+  if (line[line.length() - 1] == '0')
+    ras = true;
+  else if (line[line.length() - 1] == '1')
+    ; // LPS, great
+  else if (line[line.length() - 1] == '2')
+    throw itk::ExceptionObject(
+      __FILE__, __LINE__, "Fiducials file with IJK coordinates is not supported", __FUNCTION__);
+  else
+    throw itk::ExceptionObject(__FILE__, __LINE__, "Unrecognized coordinate system", __FUNCTION__);
+  std::getline(pointsFile, line); //# columns = id,x,y,z,ow,ox,oy,oz,vis,sel,lock,label,desc,associatedNodeID
+
+  std::vector<PointType> points;
+  std::getline(pointsFile, line);
+  while (!pointsFile.eof())
+  {
+    if (!pointsFile.good())
+      break;
+    PointType         p;
+    std::stringstream iss(line);
+
+    std::string val;
+    std::getline(iss, val, ','); // ignore ID
+    for (int col = 0; col < 3; ++col)
+    {
+      std::getline(iss, val, ',');
+      if (!iss.good())
+        break;
+
+      std::stringstream convertor(val);
+      convertor >> p[col];
+
+      if (ras && col < 2)
+        p[col] *= -1;
+    }
+    points.push_back(p);
+    std::getline(pointsFile, line);
+  }
+  return points;
+}
 
 template <typename ImageType>
 void
@@ -56,9 +105,11 @@ mainProcessing(std::string inputFileName, std::string outFilename, std::string a
   using RegionType = typename LabelImageType::RegionType;
   using IndexType = typename LabelImageType::IndexType;
   using SizeType = typename LabelImageType::SizeType;
+  using PointType = typename ImageType::PointType;
 
   typename ImageType::Pointer inImage = ReadImage<ImageType>(inputFileName);
   typename ImageType::Pointer atlasBone1 = ReadImage<ImageType>(atlasDirectory + "/907-L-bone1.nrrd");
+  std::vector<PointType> aLandmarks = readSlicerFiducials(atlasDirectory + "/907-L.fcsv");
 
   double avgSpacing = 1.0;
   for (unsigned d = 0; d < Dimension; d++)
