a538e7fdf6f69f4b97562bbb2450c8462ddcb362
Dženan Zukić <dzenan.zukic@kitware.com>
2022-02-24 16:06:09 -0500

    Let's redo all 4 atlases

--- src/hasi/segmentation_pipeline.py
+++ src/hasi/segmentation_pipeline.py
@@ -311,10 +311,10 @@ if __name__ == '__main__':
     if len(sys.argv) == 6:  # this is the subprocess call
         process_case(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]), sys.argv[5])
     elif len(sys.argv) == 1:  # direct invocation
-        # main_processing('../../', 'Tibia', '901-R', 2)
-        main_processing('../../', 'Tibia', '901-L', 2)
         main_processing('../../', 'Femur', '907-L', 1)
         main_processing('../../', 'Femur', '907-R', 1)
+        main_processing('../../', 'Tibia', '901-L', 2)
+        main_processing('../../', 'Tibia', '901-R', 2)
         # TODO: add for loops here to do this for all available atlases
     else:
         print(f'Invalid number of arguments: {len(sys.argv)}. Invoke the script with no arguments.')
